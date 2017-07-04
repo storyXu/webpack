ef6104b56f84a58bfb5c77b6bd6e392dcbf2526d
luoqiang <lq9328@126.com>
2017-07-04 21:47:31 +0800

    注释语句

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+  	index: './app/index.js',
+  	index2: './app/index2.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
